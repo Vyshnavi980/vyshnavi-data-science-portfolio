72c1f3af34a6f14ae6737e2c1b6871a177a14c55
Vyshnavi M R <vmr@lululemon.com>
2025-04-14 13:54:13 +0530

    Fixed image path for streamlit

--- Portfolio.py
+++ Portfolio.py
@@ -25,7 +25,7 @@ if selected_section == "👋 Home":
     with st.container():
         col1, col2 = st.columns([1, 4])
         with col1:
-            st.image("images/Vyshnavi.jpeg", caption="Vyshnavi MR")
+            # st.image("images/Vyshnavi.jpeg", caption="Vyshnavi MR")
   # Replace with your photo URL
         with col2:
             st.markdown("""
