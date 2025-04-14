3c801ffd1db8de8af0e40e0d35beeb578c4dfa83
Vyshnavi M R <vmr@lululemon.com>
2025-04-14 14:34:37 +0530

    Updated profile summary with one-column layout and added a box for display

--- Portfolio.py
+++ Portfolio.py
@@ -155,7 +155,7 @@ elif selected_section == "📂 Projects":
     with st.container():
         col1, col2 = st.columns([1, 2])
         with col1:
-            st.image("https://media.licdn.com/dms/image/v2/C5612AQGcjk6BelHZ2w/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1613318203099?e=2147483647&v=beta&t=BKxkdNw0kUNB93x_ZXRrGY5JVAvjknMctTo6WG25-Ps", width=100)
+            st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Time_Series_Analysis.png/800px-Time_Series_Analysis.png", width=100)
         with col2:
             st.markdown("#### 📦 Forecasting Optimization for Supply Chain")
             st.markdown("""
